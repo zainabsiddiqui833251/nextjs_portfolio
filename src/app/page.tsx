52d4d62537c4915546a04575844925c05fc3a4e3
Zainab Siddiqui <zs833251@gmail.com>
2024-09-20 15:33:35 +0500

    update all files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from '../components/Header'
 import About from '../components/About'
 import Projects from '../components/Projects'
 import Contact from '../components/Contact'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 export default function Home() {
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const Footer = () => {
     return (
