f1dfd48a4d54e8f5be156bd3a36a35673d40acde
Zainab Siddiqui <zs833251@gmail.com>
2024-09-30 16:38:33 +0500

    commit 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,30 +4,31 @@ import "./globals.css";
 import Navbar from '../components/Navbar';
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Created Using Next JS",
+  title: "Zainab Siddiqui",
+  description: "Frontend Developer",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="en"> {/* It's good practice to set the language attribute */}
       <head>
-      <link
+        <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"
         />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <title>Zainab Siddiqui</title>
       </head>
       <body>
         <Navbar />
-        <div>{children}</div>
-
+        
+          {children} {/* Render the main content */}
+        
       </body>
     </html>
   );
 }
-
-
