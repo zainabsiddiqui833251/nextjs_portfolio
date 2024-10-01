74b5e3c8dad75ede97411d3784e4b0605075be55
Zainab Siddiqui <zs833251@gmail.com>
2024-10-01 16:07:11 +0500

    commit 8

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
                 <div className="social-links">
                     <ul className='flex justify-start  items-center gap-6' >
                         <li className='pb-2'><Link href="https://x.com/sidd94699?t=7F4nRv05KgvFdfHVYx9Klw&s=09" className='text-[1.5vw]'><i className="bi bi-twitter text-[2vw] hover:text-[#a09f9f] mr-[2%]"></i></Link></li>
-                        <li className='pb-2'><Link href="https://www.linkedin.com/in/zainab-siddiqui/" className='text-[1.5vw]'><i className="bi bi-linkedin text-[2vw] hover:text-[#a09f9f] mr-[2%]"></i></Link></li>
+                        <li className='pb-2'><Link href="https://www.linkedin.com/in/zainab-siddiqui-822139283?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className='text-[1.5vw]'><i className="bi bi-linkedin text-[2vw] hover:text-[#a09f9f] mr-[2%]"></i></Link></li>
                         <li className='pb-2'><Link href="https://github.com/zainabsiddiqui833251" className='text-[1.5vw]'><i className="bi bi-github text-[2vw] hover:text-[#a09f9f] mr-[2%]"></i></Link></li>
                         <li className='pb-2'><Link href="https://t.me/zs833251" className='text-[1.5vw]'><i className="bi bi-telegram text-[2vw] hover:text-[#a09f9f] mr-[2%]"></i></Link></li>
                         <li className='pb-2'><Link href="https://wa.me/923138659252" className='text-[1.5vw]'><i className="bi bi-whatsapp text-[2vw] hover:text-[#a09f9f] mr-[2%]"></i></Link></li>
